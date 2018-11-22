b0ebc285b31e5bef6f3b93a74f6c0febf10b8f08
mdzyaan <mdzyaan@gmail.com>
2018-11-22 19:47:55 +0530

    boilerplate

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/main.scss';
+
+
+const jsx = (
+    <div>
+        <h1>Hello</h1>
+    </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+// const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+// module.exports = () => {
+//     const CSSExtract = new ExtractTextPlugin('styles.css');
+//     return {
+//         entry: './src/app.js',
+//         output: {
+//             path: path.join(__dirname, 'public', 'dist'),
+//             filename: 'bundle.js',
+//         },
+//         module: {
+//             rules: [{
+//                 loader: 'babel-loader',
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//             }, {
+//                 test: /\.s?css$/,
+//                 use: CSSExtract.extract({
+//                     use: [
+//                         {
+//                             loader: 'css-loader',
+//                             options: {
+//                                 sourceMap: true
+//                             }
+//                         },
+//                         {
+//                             loader: 'sass-loader',
+//                             options: {
+//                                 sourceMap: true
+//                             }
+//                         }
+//                     ]
+//                 })
+//             }]
+//         },
+//         devtool: 'inline-source-map',
+//         devServer: {
+//         contentBase: path.join(__dirname, 'public'),
+//         historyApiFallback: true,
+//         publicPath: '/dist/'
+//         }
+
+//     }
+// }
+
+
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+const CSSExtract = new ExtractTextPlugin('styles.css');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },{
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+      }],
+    },
+    plugins: [
+        CSSExtract,   
+    ],
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true,
+    }
+}
